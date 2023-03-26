fffcd0fb6b0b216d52bd1bc974d99e7f460875f4
mabeh19 <mathiasibehrens@gmail.com>
2023-03-26 11:16:48 +0200

    Small change in dir structure

--- lua/e_gdb/init.lua
+++ lua/e_gdb/init.lua
@@ -1,2 +1,12 @@
+local session = require "session"
 
+local M = {}
+
+function M.setup()
+        vim.api.nvim_create_user_command("EGDBLaunch", 'require("e_gdb").launch()', {})
+end
+
+M.launch = session.launch
+
+return M
 
