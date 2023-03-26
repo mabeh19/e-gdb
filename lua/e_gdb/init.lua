1c67b3dda3f9e7ff5f335a32c8d903ded83f4c75
mabeh19 <mathiasibehrens@gmail.com>
2023-03-26 11:18:22 +0200

    Updated command

--- lua/e_gdb/init.lua
+++ lua/e_gdb/init.lua
@@ -3,7 +3,7 @@ local session = require "session"
 local M = {}
 
 function M.setup()
-        vim.api.nvim_create_user_command("EGDBLaunch", 'require("e_gdb").launch()', {})
+        vim.api.nvim_create_user_command("EGDBLaunch", 'lua require("e_gdb").launch()', {})
 end
 
 M.launch = session.launch
