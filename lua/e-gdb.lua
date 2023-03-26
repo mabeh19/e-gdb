3efcb2bb48f7a28b84b3888992d2bd5ea052a7fb
mabeh19 <mathiasibehrens@gmail.com>
2023-03-26 10:44:56 +0200

    removed plugin folder

--- lua/e-gdb.lua
+++ lua/e-gdb.lua
@@ -1,10 +1,10 @@
-
-
-local session = require("e-gdb.session")
+local session = require "e-gdb.session"
 
 local M = {}
 
-vim.api.nvim_create_user_command("EGDBLaunch", 'require("e-gdb").launch()')
+function M.setup()
+        vim.api.nvim_create_user_command("EGDBLaunch", 'require("e-gdb.session").launch()', {})
+end
 
 M.launch = session.launch
 
